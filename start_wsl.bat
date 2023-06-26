105e9f84f1e99e569f2eb1f4afe82078d885fd1c
Mike Hopkins <mike@dthought.net>
2023-06-26 16:45:53 +1000

    Enable WSL2 env variable passing

--- start_wsl.bat
+++ start_wsl.bat
@@ -4,6 +4,10 @@ cd /D "%~dp0"
 
 set PATH=%PATH%;%SystemRoot%\system32
 
+@rem Uncomment the two following lines to enable OOBABOOGA_FLAGS settings between Windows and WSL
+@rem set OOBABOOGA_FLAGS=--chat
+@rem set WSLENV=%WSLENV%:OOBABOOGA_FLAGS
+
 @rem sed -i 's/\x0D$//' ./wsl.sh converts newlines to unix format in the wsl script
 call wsl -e bash -lic "sed -i 's/\x0D$//' ./wsl.sh; source ./wsl.sh"
 
